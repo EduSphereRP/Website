d34725fbbab143a6a2dec9130c69a0bd65bc7238
Friezer <hello@friezer.tech>
2024-03-18 01:32:38 +0100

    Add required files

---
+++ nuxt.config.js
@@ -0,0 +1,24 @@
+export default {
+    // Options de build
+    build: {
+      // ...
+    },
+  
+    // Routage
+    router: {
+      // ...
+    },
+  
+    // Serveur
+    server: {
+      // ...
+    },
+  
+    // Modules
+    modules: [
+      // ...
+    ],
+  
+    // ...autres options
+  }
+  
